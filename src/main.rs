91b6f769e225b146248f9ddee1080b1f746b429c
tstellanova <tstellanova@users.noreply.github.com>
2019-07-27 10:19:05 -0700

    Move main event processing into tracker
    - Move SAE rendering into tracker

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use rayon::prelude::*;
 use image::{ RgbImage };
 
 #[macro_use]
 extern crate clap;
 
-//use std::io::Read;
 use std::io::BufReader;
-//use byteorder::{LittleEndian, ReadBytesExt};
-//use rand::{ Rng};
-
-
-use arcstar::detector::*;
 use arcstar::sae_types::*;
 use eventcam_tracker::tracker::FeatureTracker;
 
@@ -28,17 +21,7 @@ pub const BLUE_PIXEL: [u8; 3] = [0,0,  255u8];
 
 
 
-pub fn render_matches(nrows: u32, ncols: u32, matches: &Vec<(SaeEvent,SaeEvent)> ) -> RgbImage {
-  let mut out_img =   RgbImage::new(ncols, nrows);
 
-  for (new_evt, old_evt) in matches {
-    out_img.put_pixel(new_evt.col as u32, new_evt.row as u32, image::Rgb(RED_PIXEL));
-    out_img.put_pixel(old_evt.col as u32, old_evt.row as u32, image::Rgb(GREEN_PIXEL));
-
-  }
-
-  out_img
-}
 
 /// render corners into an image result
 pub fn render_corners(nrows: u32, ncols: u32, events: &Vec<SaeEvent> ) -> RgbImage {
@@ -57,60 +40,6 @@ pub fn render_corners(nrows: u32, ncols: u32, events: &Vec<SaeEvent> ) -> RgbIma
   out_img
 }
 
-pub fn render_sae_frame(nrows: u32, ncols: u32, sae_rise: &SaeMatrix, sae_fall: &SaeMatrix, time_horizon: SaeTime ) -> RgbImage {
-  let mut out_img =   RgbImage::new(ncols, nrows);
-
-  for row in 0..nrows {
-    for col in 0..ncols {
-      let sae_rise_val: SaeTime = sae_rise[(row as usize, col as usize)] ;
-      let sae_fall_val: SaeTime = sae_fall[(row as usize, col as usize)] ;
-      let sae_val = sae_rise_val.max(sae_fall_val);
-      if 0 != sae_val && sae_val > time_horizon {
-        let blue_val = 0;
-        let total_val = (sae_fall_val + sae_rise_val) as f32;
-        let red_val: u8 = (255.0*(sae_rise_val as f32)/total_val) as u8;
-        let green_val: u8 = (255.0*(sae_fall_val as f32)/total_val) as u8;
-
-        let px_data: [u8; 3] = [red_val, green_val, blue_val];
-        let px =  image::Rgb(px_data);
-
-        out_img.put_pixel(col as u32, row as u32, px);
-      }
-    }
-  }
-
-  out_img
-}
-
-fn process_events(tracker: &mut FeatureTracker, sae_rise: &mut SaeMatrix,  sae_fall: &mut SaeMatrix, event_list: &Vec<SaeEvent>) -> Vec<(SaeEvent, SaeEvent)>  {
-
-  //update the SAE first //TODO handle one event at at a time?
-  for evt in event_list {
-    let row = evt.row as usize;
-    let col = evt.col  as usize;
-    match evt.polarity {
-      1 => sae_rise[(row, col)] = evt.timestamp,
-      _ => sae_fall[(row, col)] = evt.timestamp,
-    };
-  }
-
-  // we can check corners in parallel because SAE has already been updated,
-  // and can now be considered read-only
-  let corners:Vec<SaeEvent> = event_list.par_iter().filter_map(|evt| {
-    detect_and_compute_one(&sae_rise, &sae_fall, evt)
-  }).collect();
-
-  let matches:Vec<(SaeEvent, SaeEvent)> = corners.iter().filter_map(|new_evt: &SaeEvent| {
-    let matched_parent = tracker.add_and_match_feature(&new_evt);
-    match matched_parent.is_some() {
-      true => Some( (new_evt.clone(), matched_parent.unwrap()) ),
-      false => None
-    }
-  }).collect();
-
-  println!("events: {} corners: {} matches: {}", event_list.len(), corners.len(), matches.len() );
-  matches
-}
 
 
 /// process an event file into a set of corners
@@ -122,24 +51,11 @@ pub fn process_event_file(src_path: &Path, img_w: u32, img_h: u32,  render_sae: 
     return;
   }
   let event_file = event_file_res.unwrap();
-  let mut buf_reader = BufReader::new(event_file);
-
-  // The Surface of Active Events (timestamps for last event at each pixel point)
-  let mut  sae_rise = SaeMatrix::zeros(
-    img_h as usize, // rows
-    img_w as usize // cols
-  );
-
-  let mut  sae_fall = SaeMatrix::zeros(
-    img_h as usize, // rows
-    img_w as usize // cols
-  );
-
-  let mut tracker = Box::new(FeatureTracker::new());
+  let mut event_reader = BufReader::new(event_file);
+  let mut tracker = Box::new(FeatureTracker::new(img_w, img_h));
 
   //ensure that output directory exists
   create_dir_all(Path::new("./out/")).expect("Couldn't create output dir");
-
 
   let mut chunk_count = 0;
   loop {
@@ -151,14 +67,14 @@ pub fn process_event_file(src_path: &Path, img_w: u32, img_h: u32,  render_sae: 
     let frame_time_delta:SaeTime = 10000; // 10 ms / 0.01 sec given the timescale above
     let max_time_delta:SaeTime = 5*frame_time_delta;
 
-    let event_list_opt = conversion::read_next_chunk_sae_events(&mut buf_reader, timebase, timescale);
+    let event_list_opt = conversion::read_next_chunk_sae_events(&mut event_reader, timebase, timescale);
     if event_list_opt.is_none() {
       break;
     }
     let event_list = event_list_opt.unwrap();
 
     if event_list.len() > 0 {
-      let matches:Vec<(SaeEvent,SaeEvent)> = process_events(&mut tracker, &mut sae_rise, &mut sae_fall, &event_list);
+      let matches:Vec<(SaeEvent,SaeEvent)> = tracker.process_events(&event_list);
 
       let timestamp = event_list.first().unwrap().timestamp;
       let horizon = timestamp.max(max_time_delta) - max_time_delta;
@@ -171,7 +87,8 @@ pub fn process_event_file(src_path: &Path, img_w: u32, img_h: u32,  render_sae: 
       }
 
       if render_sae {
-        let out_img = render_sae_frame(img_h, img_w, &sae_rise, &sae_fall, horizon);
+        let out_img = tracker.render_sae_frame(horizon);
+        //let out_img = render_sae_frame(img_h, img_w, &sae_rise, &sae_fall, horizon);
         let out_path = format!("./out/saesurf_{:04}.png", chunk_count);
         out_img.save(out_path).expect("Couldn't save");
       }
@@ -221,7 +138,7 @@ fn main() {
   let render_sae = matches.is_present("RNDR_SAE");
   let render_matches = matches.is_present("RNDR_MATCHES");
   let render_tracks = matches.is_present("RNDR_TRACKS");
-  
+
   println!("render_tracks: {}", render_tracks);
 
   let in_path = Path::new(infile);
