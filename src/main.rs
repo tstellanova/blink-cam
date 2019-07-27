4a62f32eb84866a9c8821dede45deef0c19a1414
tstellanova <tstellanova@users.noreply.github.com>
2019-07-27 09:37:00 -0700

    Add command line configuration options for rendering etc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use rayon::prelude::*;
 use image::{ RgbImage };
 
+#[macro_use]
+extern crate clap;
+
 //use std::io::Read;
 use std::io::BufReader;
 //use byteorder::{LittleEndian, ReadBytesExt};
@@ -54,7 +57,7 @@ pub fn render_corners(nrows: u32, ncols: u32, events: &Vec<SaeEvent> ) -> RgbIma
   out_img
 }
 
-pub fn render_sae(nrows: u32, ncols: u32, sae_rise: &SaeMatrix, sae_fall: &SaeMatrix,  time_horizon: SaeTime ) -> RgbImage {
+pub fn render_sae_frame(nrows: u32, ncols: u32, sae_rise: &SaeMatrix, sae_fall: &SaeMatrix, time_horizon: SaeTime ) -> RgbImage {
   let mut out_img =   RgbImage::new(ncols, nrows);
 
   for row in 0..nrows {
@@ -63,16 +66,11 @@ pub fn render_sae(nrows: u32, ncols: u32, sae_rise: &SaeMatrix, sae_fall: &SaeMa
       let sae_fall_val: SaeTime = sae_fall[(row as usize, col as usize)] ;
       let sae_val = sae_rise_val.max(sae_fall_val);
       if 0 != sae_val && sae_val > time_horizon {
-        let mut blue_val = 0;
+        let blue_val = 0;
         let total_val = (sae_fall_val + sae_rise_val) as f32;
-        let mut red_val: u8 = (255.0*(sae_rise_val as f32)/total_val) as u8;
-        let mut green_val: u8 = (255.0*(sae_fall_val as f32)/total_val) as u8;
-//        if red_val < 255 && green_val < 255 {
-//          // these are both rising and falling
-//          blue_val = 255;
-//          red_val = 0;
-//          green_val = 0;
-//        }
+        let red_val: u8 = (255.0*(sae_rise_val as f32)/total_val) as u8;
+        let green_val: u8 = (255.0*(sae_fall_val as f32)/total_val) as u8;
+
         let px_data: [u8; 3] = [red_val, green_val, blue_val];
         let px =  image::Rgb(px_data);
 
@@ -116,7 +114,7 @@ fn process_events(tracker: &mut FeatureTracker, sae_rise: &mut SaeMatrix,  sae_f
 
 
 /// process an event file into a set of corners
-pub fn process_event_file(src_path: &str, img_w: u32, img_h: u32, render_out: bool) {
+pub fn process_event_file(src_path: &Path, img_w: u32, img_h: u32,  render_sae: bool, render_matches: bool, render_tracks: bool) {
 
   let event_file_res = std::fs::File::open(src_path);
   if event_file_res.is_err() {
@@ -147,6 +145,7 @@ pub fn process_event_file(src_path: &str, img_w: u32, img_h: u32, render_out: bo
   loop {
     chunk_count += 1;
 
+    //TODO allow timebase, scale to be defined from command line
     let timebase:f64 = 0.003811000; //from slider events.txt file -- //TODO standardize
     let timescale:f64 = 1E-6; //one microsecond per SaeTime tick
     let frame_time_delta:SaeTime = 10000; // 10 ms / 0.01 sec given the timescale above
@@ -159,29 +158,28 @@ pub fn process_event_file(src_path: &str, img_w: u32, img_h: u32, render_out: bo
     let event_list = event_list_opt.unwrap();
 
     if event_list.len() > 0 {
-      let _matches:Vec<(SaeEvent,SaeEvent)> = process_events(&mut tracker, &mut sae_rise, &mut sae_fall, &event_list);
+      let matches:Vec<(SaeEvent,SaeEvent)> = process_events(&mut tracker, &mut sae_rise, &mut sae_fall, &event_list);
 
-      //TODO fix rendering
-//      if render_out {
-//        let lead_events = matches.iter().map(|(new, _old)| new.clone()).collect();
-//        let out_img = render_corners(img_h, img_w, &lead_events);
-//        let out_path = format!("./out/sae_{:04}_evts.png", chunk_count);
-//        out_img.save(out_path).expect("Couldn't save");
-//      }
+      let timestamp = event_list.first().unwrap().timestamp;
+      let horizon = timestamp.max(max_time_delta) - max_time_delta;
 
-      if render_out {
-        let timestamp = event_list.first().unwrap().timestamp;
-        let horizon = timestamp.max(max_time_delta) - max_time_delta;
-        let out_img = render_sae(img_h, img_w, &sae_rise, &sae_fall, horizon);
+      if render_matches {
+        let lead_events = matches.iter().map(|(new, _old)| new.clone()).collect();
+        let out_img = render_corners(img_h, img_w, &lead_events);
+        let out_path = format!("./out/sae_{:04}_evts.png", chunk_count);
+        out_img.save(out_path).expect("Couldn't save");
+      }
+
+      if render_sae {
+        let out_img = render_sae_frame(img_h, img_w, &sae_rise, &sae_fall, horizon);
         let out_path = format!("./out/saesurf_{:04}.png", chunk_count);
         out_img.save(out_path).expect("Couldn't save");
       }
 
-//      if render_out {
-//        let out_path = format!("./out/sae_{:04}_tracks.png", chunk_count);
-//        let lead_time_horizon = timestamp.max(FORGETTING_TIME) - FORGETTING_TIME;
-//        tracker.render_tracks_to_file(img_h, img_w,  lead_time_horizon, &out_path); //TODO check timestamp
-//      }
+      if render_tracks {
+        let out_path = format!("./out/sae_{:04}_tracks.png", chunk_count);
+        tracker.render_tracks_to_file(img_h, img_w,  horizon, &out_path); //TODO check timestamp
+      }
       
     }
     else {
@@ -201,11 +199,40 @@ pub fn process_event_file(src_path: &str, img_w: u32, img_h: u32, render_out: bo
 
 
 fn main() {
-  //TODO get image / SAE dimensions from configuration?
-  let img_w = 240;
-  let img_h = 180;
 
-  process_event_file("./data/events.dat", img_w, img_h, true);
+  let matches = clap_app!(blink_cam =>
+        (version: "0.1.0")
+        (author: "Todd Stellanova")
+        (about: "Process event camera pixel change event stream into tracked features.")
+        (@arg INPUT: -i --input +takes_value  "Sets the input file to use: default is ./data/events.dat")
+        (@arg WIDTH: --width +takes_value  "Sets the input width to use: default is 240 pixels")
+        (@arg HEIGHT: --height +takes_value  "Sets the input height to use: default is 180 pixels")
+        (@arg RNDR_SAE: --rend_sae  "Render the Surface of Active Events (SAE)")
+        (@arg RNDR_MATCHES: --rend_matches   "Render feature matches as points")
+        (@arg RNDR_TRACKS: --rend_tracks   "Render feature matches as tracks:")
+    ).get_matches();
+
+  println!("clap matches: {:?}", matches);
+
+  let infile = matches.value_of("INPUT").unwrap_or("./data/events.dat");
+  let img_w = matches.value_of("WIDTH").unwrap_or("240").parse::<u32>().unwrap();
+  let img_h = matches.value_of("HEIGHT").unwrap_or("180").parse::<u32>().unwrap();
+
+  let render_sae = matches.is_present("RNDR_SAE");
+  let render_matches = matches.is_present("RNDR_MATCHES");
+  let render_tracks = matches.is_present("RNDR_TRACKS");
+  
+  println!("render_tracks: {}", render_tracks);
+
+  let in_path = Path::new(infile);
+  if !in_path.exists() {
+    eprintln!("Input file doesn't exist: {}", infile);
+  }
+  else {
+    println!("Reading from {}", infile);
+  }
+
+  process_event_file(&in_path, img_w, img_h, render_sae, render_matches, render_tracks);
 
 }
 
