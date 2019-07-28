4753ef10165a41af9cad120d990b6b5158a6ba47
tstellanova <tstellanova@users.noreply.github.com>
2019-07-28 09:37:13 -0700

    Simplify interface between app, tracker, detector

---
+++ vid_tracks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# convert multiple png files to a video file
+# requires that libx264 and ffmpeg are installed
+rm sae_tracks.mp4
+
+ffmpeg -r 24 \
+ -f image2 \
+ -start_number 1 \
+ -i ./out/sae_%04d_tracks.png \
+ -vcodec libx264 \
+ -crf 25 \
+ -pix_fmt yuv420p \
+ sae_tracks.mp4
